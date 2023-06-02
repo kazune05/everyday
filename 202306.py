f639ad6956c84999f40ffca87792960a1f978546
kazune05 <kazu05info@gmail.com>
2023-06-02 14:27:01 +0900

    0602

--- 202306.py
+++ 202306.py
@@ -4,3 +4,12 @@ s=''
 for i in range(input_line):
     s= s +'*' 
 print(s)
+
+#0602:C084:枠で囲む
+input_line = input()
+s_len= len(input_line)
+for i in range (s_len):
+    s = '+'* (s_len+2)+'\n'
+    s2= '+'+ input_line +'+\n'
+    s3=  '+'* (s_len+2)
+print(s+s2+s3)
