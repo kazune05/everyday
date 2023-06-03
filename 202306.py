713cea2053d50bd32b5fea5c398d566e52d78a6e
kazune05 <kazu05info@gmail.com>
2023-06-03 21:40:24 +0900

    0603

--- 202306.py
+++ 202306.py
@@ -13,3 +13,7 @@ for i in range (s_len):
     s2= '+'+ input_line +'+\n'
     s3=  '+'* (s_len+2)
 print(s+s2+s3)
+
+#0603:C016:Leet文字列
+input_line = input()
+print(input_line.replace('A','4').replace('E','3').replace('G','6').replace('I','1').replace('O','0').replace('S','5').replace('Z','2'))
