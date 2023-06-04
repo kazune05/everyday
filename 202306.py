3ea72e9fda9abf0c3db8a21995ea51d21c8821ff
kazune05 <kazu05info@gmail.com>
2023-06-04 20:55:14 +0900

    0604

--- 202306.py
+++ 202306.py
@@ -17,3 +17,13 @@ print(s+s2+s3)
 #0603:C016:Leet文字列
 input_line = input()
 print(input_line.replace('A','4').replace('E','3').replace('G','6').replace('I','1').replace('O','0').replace('S','5').replace('Z','2'))
+
+#0604:D174:栄養の摂取
+input_line = int(input())
+s = input_line * 1500
+print(str(s))
+
+#D204:2にする
+input_line = int(input())
+result = 222 - input_line
+print(str(result))
